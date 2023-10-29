806ac4fe30853ab5d5d0dc110ed7946748fca453
Josh <joshuamares180@gmail.com>
2023-10-28 20:29:33 -0700

    can get auth token from spotify now

--- spotify-project/src/App.tsx
+++ spotify-project/src/App.tsx
@@ -1,10 +1,24 @@
-import { useState } from "react";
-import logo from './logo.svg';
 import Message from "./Message";
 import './App.css';
+import {useState, useEffect } from "react";
+import {clientID, secretClient} from "../../spotify_keys.ts"
 
 function App(){
-  const [count, setCount] = useState(0);
+  const [accessToken, setAccessToken] = useState("");
+
+  useEffect(() => {
+    var authParameters = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body: 'grant_type=client_credentials&client_id=' + clientID + '&client_secret=' + secretClient
+    }
+    fetch('https://accounts.spotify.com/api/token', authParameters)
+      .then(result => result.json())
+      .then(data => setAccessToken(data.access_token));
+  }, [] );
+
   return <div><Message /></div>;
 }
 

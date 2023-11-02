6c8bc225f35e9db66beaa567f654a33cc548ffa4
Josh <joshuamares180@gmail.com>
2023-11-01 22:47:51 -0700

    Created navbar and set spotify color pallete

--- spotify-project/src/Navbar.tsx
+++ spotify-project/src/Navbar.tsx
@@ -1,14 +1,13 @@
 import './Navbar.css'
 
 const Navbar = () => {
-    return ( <nav className="navbar">
-        <h1>Spotify Party Blend</h1>
-        <div className="links">
-            <a href="/">Home</a>
-            <a href="/Logout">Log Out</a>
-            <a href="/Settings">Settings</a>
-        </div>
+    return ( 
+    <nav className="navbar">
+        <h2><a href="/" className='site-title'>Spotify Party Blend</a></h2>
+        <ul className='links'>
+            <li><a href="/Settings">Settings</a></li>
+        </ul>
     </nav> );
 }
- 
+ //look into this when creating settingshttps://www.youtube.com/watch?v=SLfhMt5OUPI&t=93s&ab_channel=WebDevSimplified
 export default Navbar;

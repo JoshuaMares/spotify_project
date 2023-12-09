9f91c04fc05b87d3739b00643f83bd8c0f259be3
Josh <joshuamares180@gmail.com>
2023-12-08 23:10:25 -0800

    setting files

--- spotify-project-node-server/server.js
+++ spotify-project-node-server/server.js
@@ -1,9 +1,13 @@
 const http = require('http');
 
-const server = http.createServer((req, resp) => {
+const server = http.createServer((req, res) => {
     //callback runs everytime a request comes  in
     //request and response object
     console.log('request made');
+    console.log(req.url, req.method);
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<p>hello, spotify users</p>');
+    res.end();
 });
 
 server.listen(3000, 'localhost', ()=>{

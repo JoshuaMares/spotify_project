c097c169d5d4d95b75e342fd656ab240b094b927
Josh <joshuamares180@gmail.com>
2023-12-17 23:49:08 -0800

    fix failing fetch in getTokens in fronthend

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 require('dotenv').config()
+const client_keys = require('../private_info/spotify_keys.ts');
 const express = require('express');
 
 const app = express();
@@ -13,3 +14,24 @@ app.use((req, res, next) => {
 app.get('/', (req, res) => {
     res.json({'mssg': 'Welcome to the app'});
 });
+
+app.post('/callback', (req, res) => {
+    console.log('body: ', req.body);
+    const code = req.query.code || null;
+    console.log('code: ', code)
+
+    const authOptions = {
+    url: 'https://accounts.spotify.com/api/token',
+    form: {
+        code: code,
+        redirect_uri: 'http://localhost:5173/loading/',
+        grant_type: 'authorization_code'
+    },
+    headers: {
+        'content-type': 'application/x-www-form-urlencoded',
+        'Authorization': 'Basic ' + (new Buffer.from(client_keys.id + ':' + client_keys.secret).toString('base64'))
+    },
+    json: true
+    };
+    console.log('authOptions: ', authOptions);
+})
--- frontend/spotify-project/src/spotify_functions.tsx
+++ frontend/spotify-project/src/spotify_functions.tsx
@@ -1,5 +1,4 @@
 import { json } from "react-router-dom";
-import {clientID, secretClient} from "../../../private_info/spotify_keys.ts";
 import {useState, useEffect } from "react";
 const HTML_URL_SPACE_ENCODING = '%20';
 
@@ -12,11 +11,26 @@ when user allows access the spotify page uses the redirect_uri we
 then take code and use it to request access and refresh token
 */
 
+const BACKEND_URL = 'http://localhost:4000/';
 const REDIRECT_URI = 'http://localhost:5173/loading/';
 const HOME_URL = 'http://localhost:5173/home/';
 const LANDING_URL = 'http://localhost:5173/';
 const AUTHORIZE_URL = 'https://accounts.spotify.com/authorize?';
 const TOKEN_URL = 'https://accounts.spotify.com/api/token';
+const CLIENT_ID = 'a70182fad1a1414f9f3529dd9f018f8d';
+
+//auth flow
+/*
+assuming we arent storing tokens
+get to landing page
+click on log in to spotify
+user redirects to request authorization page provided by spotify
+    (just need client id and permissions)
+click accept which redirects back to our page with the authorization code
+we then make a call to our back end passing the authorization code
+our node server then uses the secret key only it has to get the tokens
+the tokens are then passed back to the front end
+*/
 
 function requestAuthorization(){
     //scopes are permissions we want
@@ -31,12 +45,13 @@ function requestAuthorization(){
                   'user-library-read', //read other items in library (likes songs, albums, podcasts, etc)
                   'user-read-private']; //read profile
     let url = AUTHORIZE_URL;
-    url += "client_id=" + clientID;
+    url += "client_id=" + CLIENT_ID;
     url += "&response_type=code";
     url += "&redirect_uri=" + encodeURI(REDIRECT_URI);
     url += "&show_dialog=true";
     url += "&scope=" + scopes.join(HTML_URL_SPACE_ENCODING);
-    window.location.href = url;   
+    window.location.href = url;
+    //look into state parameter   
 }
 
 function useTokens(){
@@ -44,20 +59,12 @@ function useTokens(){
         if(window.location.search.length > 0){
             let code = getCode();
             const abortCont = new AbortController();
-            let authParameters = {
+            let fetchParameters = {
                 method: 'POST',
-                headers: {
-                    'Content-Type': 'application/x-www-form-urlencoded',
-                    'Authorization': 'Basic ' + btoa(clientID + ':' + secretClient)
-                },
-                body: ('grant_type=authorization_code' 
-                    + '&code=' + code
-                    + '&redirect_uri=' + encodeURI(REDIRECT_URI)
-                    + '&client_id=' + clientID
-                    + "&client_secret=" + secretClient),
+                body: ('code=' + code),
                 signal: abortCont.signal
             }
-            fetch(TOKEN_URL, authParameters)
+            fetch(BACKEND_URL + 'callback', fetchParameters)
             .then(result => {
                 if(!result.ok){
                     throw Error('could not fetch data for that resource');
@@ -82,6 +89,50 @@ function useTokens(){
         }
     }, []);
 }
+
+// function useTokens(){
+//     useEffect(() => {
+//         if(window.location.search.length > 0){
+//             let code = getCode();
+//             const abortCont = new AbortController();
+//             let authParameters = {
+//                 method: 'POST',
+//                 headers: {
+//                     'Content-Type': 'application/x-www-form-urlencoded',
+//                     'Authorization': 'Basic ' + btoa(clientID + ':' + secretClient)
+//                 },
+//                 body: ('grant_type=authorization_code' 
+//                     + '&code=' + code
+//                     + '&redirect_uri=' + encodeURI(REDIRECT_URI)
+//                     + '&client_id=' + clientID
+//                     + "&client_secret=" + secretClient),
+//                 signal: abortCont.signal
+//             }
+//             fetch(TOKEN_URL, authParameters)
+//             .then(result => {
+//                 if(!result.ok){
+//                     throw Error('could not fetch data for that resource');
+//                 }
+//                 return result.json()
+//             }).then(data => {
+//                 localStorage.setItem("accessToken", data.access_token);
+//                 console.log("accessToken: " + data.access_token);
+//                 localStorage.setItem("refreshToken", data.refreshToken);
+//                 console.log("refreshToken: " + data.refresh_token);
+//                 window.location.href = HOME_URL; 
+//                 //window.history.pushState("", "", REDIRECT_URI);
+//             }).catch(err => {
+//                 if(err.name === 'AbortError'){
+//                     console.log('fetch aborted while getting tokens');
+//                 }else{
+//                     console.log(err.message);
+//                     alert(err.message);
+//                 }
+//             });
+//             return () => abortCont.abort();
+//         }
+//     }, []);
+// }
 
 function getLocalStorageAccessToken(){
     let lsat = localStorage.getItem('accessToken');
